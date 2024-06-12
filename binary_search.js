e2e8c0f5b5f23732f549edc597dae219ee0ee15f
Bogdan <bogdanbuterchi99@gmail.com>
2024-06-12 22:54:38 +0200

    added binary search ex

--- binary_search.js
+++ binary_search.js
@@ -2,10 +2,10 @@
 // Return the index of the target value in the array
 // If the target value is not in the array then return -1
 
-const arr = ['a', 'b', 'c', 'd', 'x', 'z'];
+const arr = ['a', 'b', 'c', 'd', 'e', 'x', 'z'];
 
 // This function can work if the array is small but if you loop over thousands of data it not efficient
-export function search(arr, target) {
+function search(arr, target) { // using the 'export' term will allow the funtion to be called in other applications
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === target) return i;
     }
@@ -14,7 +14,7 @@ export function search(arr, target) {
 // Binary search, If you're looking for someone with the letter J in a phonebook, it's easier to just go to the middle of it
 // and from there go backwards or forwards depending where you landed
 
-export function search2(arr, target, start=0, end=arr.length-1) { //define a start and end as well
+function search2(arr, target, start=0, end=arr.length-1) { //define a start and end as well
 
     console.log(start, end);
 
@@ -23,7 +23,21 @@ export function search2(arr, target, start=0, end=arr.length-1) { //define a sta
         return -1;
     }
 
+    const middle = Math.floor((start+end) / 2) // Math.floor() will return an integer instead of a decimal value
 
-
-
+    if (arr[middle] === target) { 
+        console.log(`${target} Found at index ${middle}`);
+        return middle;        
+    }
+    
+    if (arr[middle] > target) {
+        return search2(arr, target, start, middle - 1);
+    }
+    
+    if (arr[middle] < target) {
+        return search2(arr, target, middle + 1, end);
+    }
 }
+
+console.log(search(arr, 'z'));
+console.log(search2(arr, 'd'));
